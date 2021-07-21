9fad7ca94344ac3ca7468419b3464b914d995345
Brad Teague <b_rad88@live.com>
2021-07-22 06:59:40 +0930

    hiding fullscreen image controls

--- src/components/gallery/Fullscreen.js
+++ src/components/gallery/Fullscreen.js
@@ -8,13 +8,15 @@ import { Grid } from "react-spinners-css";
 const Fullscreen = ({ image, nextImage, previousImage, exit }) => {
   const [loading, setLoading] = useState(false);
   const [comments, setComments] = useState([]);
+  const [controlVisibility, setControlVisibility] = useState(true);
   const [commentsVisible, setCommentsVisible] = useState(false);
   useEffect(() => {
     setLoading(true);
     getImage();
     getComments();
   }, [image]);
-
+  const timer = useRef(null);
+  console.log("rerender");
   const getImage = () => {
     const img = new Image();
     img.src = image.highres;
@@ -47,17 +49,58 @@ const Fullscreen = ({ image, nextImage, previousImage, exit }) => {
     });
   };
 
+  const controlVisibilityTimer = () => {
+    getTimer();
+    setControlVisibility(false);
+  };
+
+  const onMouseMove = () => {
+    console.log("Cleared this timer:", timer.current);
+    clearTimeout(timer.current);
+    setControlVisibility(true);
+    timer.current = setTimeout(controlVisibilityTimer, 500);
+  };
+
+  const onMouseEnter = e => {
+    console.log("yeppers");
+    e.stopPropagation();
+    // console.log(controlTimer);
+    clearTimeout(timer.current);
+  };
+
+  const onMouseMoveButton = e => {
+    e.stopPropagation();
+  };
+
+  const getTimer = () => {
+    console.log("This timer ran:", timer.current);
+  };
   return (
     <div className={"fullscreen-background"}>
-      <div className={"image-viewer"} onClick={fullscreenClickHandler}>
+      <div
+        className={"image-viewer"}
+        onClick={fullscreenClickHandler}
+        onMouseMove={onMouseMove}
+        onMouseLeave={onMouseMove}
+      >
         {loading && <Grid color={"#fff"} size={120} className={"loading-spinner"} />}
         {!loading && (
           <>
             <div className={`image-content ${loading ? "loading" : ""}`}>
-              <button className={"comments"} onClick={handleMenu}>
+              <button
+                className={`comments ${controlVisibility ? "" : "hidden"}`}
+                onClick={handleMenu}
+                onMouseEnter={onMouseEnter}
+                onMouseMove={onMouseMoveButton}
+              >
                 {menu}
               </button>
-              <button className={"close"} onClick={exit}>
+              <button
+                className={`close ${controlVisibility ? "" : "hidden"}`}
+                onClick={exit}
+                onMouseEnter={onMouseEnter}
+                onMouseMove={onMouseMoveButton}
+              >
                 {close}
               </button>
               <img src={image.highres} />
@@ -72,9 +115,21 @@ const Fullscreen = ({ image, nextImage, previousImage, exit }) => {
             </div>
           </>
         )}
-        <div className={"image-controls"}>
-          <button onClick={previousImage}>{arrowLeft}</button>
-          <button onClick={nextImage}>{arrowRight}</button>
+        <div className={`image-controls ${controlVisibility ? "" : "hidden"}`}>
+          <button
+            onClick={previousImage}
+            onMouseEnter={onMouseEnter}
+            onMouseMove={onMouseMoveButton}
+          >
+            {arrowLeft}
+          </button>
+          <button
+            onClick={nextImage}
+            onMouseEnter={onMouseEnter}
+            onMouseMove={onMouseMoveButton}
+          >
+            {arrowRight}
+          </button>
         </div>
       </div>
     </div>
