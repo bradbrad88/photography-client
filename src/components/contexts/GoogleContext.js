1b92d8136fa2d4868df38731f1ff321ae692b916
Brad Teague <b_rad88@live.com>
2021-08-26 19:52:21 +0930

    gapi auth ref

--- src/components/contexts/GoogleContext.js
+++ src/components/contexts/GoogleContext.js
@@ -4,7 +4,10 @@ const Context = React.createContext();
 
 export class GoogleStore extends React.Component {
   static contextType = UserStore;
-
+  constructor(props) {
+    super(props);
+    this.auth = React.createRef();
+  }
   componentDidMount() {
     this.getAuth();
   }
@@ -16,9 +19,10 @@ export class GoogleStore extends React.Component {
           scope: "profile",
         })
         .then(() => {
-          this.auth = window.gapi.auth2.getAuthInstance();
-          this.auth.isSignedIn.listen(this.signedInListen);
-          if (this.auth.isSignedIn.get()) this.loginGoogleUser();
+          this.auth.current = window.gapi.auth2.getAuthInstance();
+          console.log("auth instanct", window.gapi.auth2.getAuthInstance());
+          this.auth.current.isSignedIn.listen(this.signedInListen);
+          if (this.auth.current.isSignedIn.get()) this.loginGoogleUser();
         })
         .catch(error => {
           console.log(error);
@@ -31,13 +35,17 @@ export class GoogleStore extends React.Component {
       const options = {
         method: "POST",
         body: JSON.stringify({
-          token: this.auth?.currentUser.get().getAuthResponse().id_token,
+          token: this.auth.current?.currentUser.get().getAuthResponse().id_token,
         }),
         headers: {
           "Content-Type": "application/json",
         },
       };
-      const res = await fetch("http://localhost:5000/auth/google", options);
+      console.log("options", options);
+      const res = await fetch(
+        `${process.env.REACT_APP_SERVER_API}/auth/google`,
+        options
+      );
       const data = await res.json();
       this.context.onUserLogin(data);
     } catch (error) {
@@ -46,12 +54,12 @@ export class GoogleStore extends React.Component {
   };
 
   onSignin = () => {
-    if (this.auth?.isSignedIn.get()) this.auth.signOut();
-    this.auth?.signIn();
+    if (this.auth.current?.isSignedIn.get()) this.auth.current.signOut();
+    this.auth.current?.signIn();
   };
 
   onSignout = () => {
-    this.auth.signOut();
+    this.auth.current.signOut();
   };
 
   render() {
