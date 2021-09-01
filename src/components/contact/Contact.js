31d297995e67fda374bd5eaf61eee2e173e1c6c6
Brad Teague <b_rad88@live.com>
2021-09-02 08:11:54 +0930

    contact form skeleton

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,7 +1,35 @@
 import React from "react";
+import "../../stylesheets/Contact.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className={"contact-form-container"}>
+      <form className={"contact-form"}>
+        <div className={"contact-header"}>
+          <h2>Let me know how I can help!</h2>
+        </div>
+        <fieldset>
+          <input className={"contact-input"} placeholder={"First Name*"}></input>
+        </fieldset>
+        <fieldset>
+          <input className={"contact-input"} placeholder={"Last Name*"}></input>
+        </fieldset>
+        <fieldset>
+          <input className={"contact-input"} placeholder={"Your Email*"}></input>
+        </fieldset>
+        {/* either or both required */}
+        <fieldset>
+          <input className={"contact-input"} placeholder={"Phone Number*"}></input>
+        </fieldset>
+        <fieldset>
+          <textarea
+            className={"contact-input area"}
+            placeholder={"How Can I Help You? Dates of events can be helpful :)"}
+          ></textarea>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
