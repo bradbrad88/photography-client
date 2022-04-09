d9b4e0539dab4866eddfce5beb93960ea185866d
Brad Teague <b_rad88@live.com>
2022-04-09 14:44:08 +0930

    sign in with google button

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,5 @@
-import React, { useState, useContext, useEffect } from "react";
-import { useHistory } from "react-router-dom";
+import React, { useState, useContext, useEffect, useRef, useMemo } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 // import GoogleContext from "contexts/GoogleContext";
 import UserContext from "contexts/UserContext";
 // import { refreshTokenSetup } from "../../utils/refreshToken";
@@ -9,17 +9,60 @@ import Signup from "./Signup";
 
 const Login = ({ msg }) => {
   const [local, setLocal] = useState(false);
-  // const googleContext = useContext(GoogleContext);
+  const [googleLoaded, setGoogleLoaded] = useState(false);
   const { profile } = useContext(UserContext);
-  const history = useHistory();
-  // useEffect(() => {
-  //   if (profile.id) {
-  //     history.push("/");
-  //   }
-  // }, [profile]);
-  // const onLoginGoogle = () => {
-  //   googleContext.onSignin();
-  // };
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://accounts.google.com/gsi/client";
+    script.id = "google-gsi";
+    document.body.appendChild(script);
+    script.onload = () => {
+      setGoogleLoaded(true);
+      window.google.accounts.id.initialize({
+        client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+        callback: handleGoogleLogin,
+      });
+      window.google.accounts.id.renderButton(
+        document.getElementById("google-sign-in"),
+        {
+          theme: "outline",
+          size: "large",
+        }
+      );
+    };
+  }, []);
+
+  const handleGoogleLogin = async res => {
+    const headers = new Headers();
+    headers.append("Content-Type", "application/x-www-form-urlencoded");
+    const params = new URLSearchParams();
+    params.append("token", res.credential);
+    const body = params.toString();
+    try {
+      const options = {
+        headers,
+        body,
+        method: "POST",
+        credentials: "include",
+      };
+      const result = await fetch(
+        process.env.REACT_APP_SERVER_API + "/auth/google",
+        options
+      );
+      const userData = await result.json();
+      console.log(userData);
+      // Send userData to context
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const loadGoogleButton = useMemo(() => {
+    if (!googleLoaded) return null;
+    return <div id="google-sign-in" className="auth google"></div>;
+  }, [googleLoaded]);
+
   return (
     <div className="container">
       <div className="text-container">{msg}</div>
@@ -29,13 +72,7 @@ const Login = ({ msg }) => {
       {local && <LocalLogin />}
       {local && <Signup />}
       <button className="auth facebook">Login with Facebook</button>
-      <button
-        className="auth google"
-        // disabled={googleContext.auth ? false : true}
-        // onClick={onLoginGoogle}
-      >
-        Login with Google
-      </button>
+      {googleLoaded && loadGoogleButton}
     </div>
   );
 };
--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 
 const Signup = () => {
+  const [givenName, setGivenName] = useState("");
+  const [familyName, setFamilyName] = useState("");
   const [email, setEmail] = useState("");
   const [pw, setPw] = useState("");
   const [vpw, setVpw] = useState("");
@@ -12,6 +14,8 @@ const Signup = () => {
       const data = new URLSearchParams();
       data.append("email", email);
       data.append("password", pw);
+      data.append("givenName", givenName);
+      data.append("familyName", familyName);
       const body = data.toString();
 
       const options = {
@@ -30,6 +34,26 @@ const Signup = () => {
   return (
     <form className="form" onSubmit={onSubmit} autoComplete="on">
       <h1>Sign Up</h1>
+      <div className="field">
+        <label htmlFor="givenName">Given Name</label>
+        <input
+          id="givenName"
+          type="text"
+          autoComplete="given-name"
+          value={givenName}
+          onChange={e => setGivenName(e.target.value)}
+        />
+      </div>
+      <div className="field">
+        <label htmlFor="familyName">Family Name</label>
+        <input
+          id="familyName"
+          type="text"
+          autoComplete="family-name"
+          value={familyName}
+          onChange={e => setFamilyName(e.target.value)}
+        />
+      </div>
       <div className="field">
         <label htmlFor="email">Email</label>
         <input
