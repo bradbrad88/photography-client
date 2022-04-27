f6a431aec9991924f14cb3ee071ca54881bc682b
Brad Teague <b_rad88@live.com>
2022-04-27 17:20:28 +0930

    button working and back styles

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -1,23 +1,37 @@
 import { useMemo } from "react";
+import { Grid, Ripple } from "react-spinners-css";
+import { back as backIcon } from "assets/svgButtons";
 
 interface Props {
-  text: string;
+  text?: string;
   onClick: (...arg: any) => void;
   className?: string;
   icon?: (size?: number) => JSX.Element;
   iconSize?: number;
   img?: string;
+  back?: boolean;
+  working?: boolean;
 }
 
-const Button = ({ onClick, className, text, icon, iconSize, img }: Props) => {
+const Button = ({
+  onClick,
+  className,
+  text = "",
+  icon,
+  iconSize,
+  img,
+  back,
+  working,
+}: Props) => {
   const classes = useMemo(() => {
+    if (back) return "back";
     const displayIcon = img || icon ? true : false;
     return `${className} ${displayIcon && "button-image"}`;
-  }, [icon, img, className]);
+  }, [icon, img, className, back]);
   return (
     <button onClick={onClick} className={classes}>
       <>
-        {(img || icon) && (
+        {(img || icon) && !back && (
           <div className="button-image">
             <>
               {img && <img src={img} alt={text + " button icon"} />}
@@ -25,7 +39,11 @@ const Button = ({ onClick, className, text, icon, iconSize, img }: Props) => {
             </>
           </div>
         )}
-        <span className="text">{text}</span>
+        {back ? (
+          backIcon(25)
+        ) : (
+          <span className="text">{working ? <Ripple color="white" size={45} /> : text}</span>
+        )}
       </>
     </button>
   );
