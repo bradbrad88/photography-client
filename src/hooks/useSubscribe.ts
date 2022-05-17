2abd2693c7f05c8f64707d6c2a69451a56fe7b24
Brad Teague <b_rad88@live.com>
2022-05-17 22:12:50 +0930

    use subscribe hook including initialize progress feature

--- src/hooks/useSubscribe.ts
+++ src/hooks/useSubscribe.ts
@@ -1,32 +1,75 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 
-interface ProgressEvent {
+export interface ProgressEvent {
   imageId: string;
-  userId: string;
   step: string;
   progress: number;
 }
 
+export interface CompleteEvent {
+  imageId: string;
+  uploadedBy: string;
+  uploadedAt: Date;
+  aspectRatio: number;
+}
+
+interface ImageIdType {
+  imageId: string;
+}
+
 const useSubscribe = () => {
   const eventRef = useRef<EventSource>();
+  // Initial progress gives us an array of all the steps involved in uploading an image.
+  // This is done so that changes to the number and name of steps involved in the upload process won't then have to be mirrored within client side code.
+  // This tackles the issue of Progress Feedback only appearing once work has begun on it.
+  // This also allows streaming much smaller packets on each update.
+  const [initProgress, setInitProgress] = useState<ProgressEvent[]>();
 
-  const statusEvent = useCallback((imageId: string, func: (data: any) => void) => {
-    return (e: MessageEvent) => {
-      const data = JSON.parse(e.data) as ProgressEvent;
-      if (data.imageId !== imageId) return;
-      func(data);
-    };
+  const subscribeEvent = useCallback(
+    <T extends ImageIdType>(imageId: string, func: (data: T) => void) => {
+      return (e: MessageEvent) => {
+        const data = JSON.parse(e.data) as T;
+        if (data.imageId !== imageId) return;
+        func(data);
+      };
+    },
+    []
+  );
+
+  const onInitialize = useCallback((e: MessageEvent) => {
+    const data = JSON.parse(e.data) as ProgressEvent[];
+    setInitProgress(data);
   }, []);
 
   const openConnection = useCallback(() => {
     if (eventRef.current) return eventRef.current;
     const url = new URL("/gallery/subscribe", process.env.REACT_APP_SERVER_API).toString();
     const es = new EventSource(url, { withCredentials: true });
+    es.addEventListener("initialize", onInitialize);
     return es;
-  }, []);
+  }, [onInitialize]);
+
+  const subscribeImage = useCallback(
+    (
+      imageId: string,
+      onProgress: (data: ProgressEvent) => void,
+      onComplete: (data: CompleteEvent) => void
+    ) => {
+      eventRef.current = openConnection();
+      eventRef.current.addEventListener(
+        "progress",
+        subscribeEvent<ProgressEvent>(imageId, onProgress)
+      );
+      eventRef.current.addEventListener(
+        "complete",
+        subscribeEvent<CompleteEvent>(imageId, onComplete)
+      );
+    },
+    [openConnection, subscribeEvent]
+  );
 
   useEffect(() => {
-    // eventRef.current = openConnection(); --Use once setup
+    eventRef.current = openConnection(); //--Use once setup
     return () => {
       closeConnection();
     };
@@ -38,12 +81,7 @@ const useSubscribe = () => {
     eventRef.current = undefined;
   };
 
-  const subscribeImage = (imageId: string, func: (data: any) => void) => {
-    eventRef.current = openConnection();
-    eventRef.current.addEventListener("progress", statusEvent(imageId, func));
-  };
-
-  return { subscribeImage, closeConnection };
+  return { subscribeImage, closeConnection, initProgress };
 };
 
 export default useSubscribe;
